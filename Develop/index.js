97f3cc6d35a974e8a798775a29a3b03c3a195f1e
Austin Luper <austin.luper@gmail.com>
2024-01-25 15:38:19 -0500

    Added an array of questions for user input to the index.js file. There is currently incorrect syntax that will need to be corrected before project submission but the user prompts themselves are acceptable at this point.

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,58 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = async () => {
+    try {
+      const answers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'projectTitle',
+          message: 'Enter your project title:',
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Enter a brief description:',
+        },
+        {
+          type: 'input',
+          name: 'installation',
+          message: 'Enter installation instructions:',
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'Enter usage information:',
+        },
+        {
+          type: 'list',
+          name: 'license',
+          message: 'Choose a license:',
+          choices: ['MIT', 'GPL', 'Apache'],
+        },
+        {
+          type: 'input',
+          name: 'contributing',
+          message: 'Enter contribution guidelines:',
+        },
+        {
+          type: 'input',
+          name: 'tests',
+          message: 'Enter test instructions:',
+        },
+        {
+          type: 'input',
+          name: 'githubUsername',
+          message: 'Enter your GitHub username:',
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'Enter your email address:',
+        },
+      ]);
+    }
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
